5c33e7130a9dd1b51b9ac8fb5cd2646439469352
iMiike1 <43544291+iMiike1@users.noreply.github.com>
2018-11-14 21:40:40 +0000

    CompanionCodeFixed

--- Assets/Scripts/Companion.cs
+++ Assets/Scripts/Companion.cs
@@ -4,117 +4,109 @@ using UnityEngine;
 
 public class Companion : MonoBehaviour {
 
+    [Header("Timers")]
+
+    [Range(0.0f, 20.0f)]
+    public float DurationTime = 5.0f;
+    private float TargetTime = 0f;
+
+    [Range(0.0f, 20.0f)]
+    public float spawnTime = 5.0f;
+    private float RespawnTime;
+    
+    [Header("CompanionSettings")]
+
+    [Range(0.0f, 2.0f)]
+    public float fallingSpeed = 0.0f;
+    [Range(0.0f, 100.0f)]
+    public float HeightOnRespawn = 0.0f;
+    [Range(-100.0f, 0.0f)]
+    public float YbeforeRespawn = 0.0f;
+
+
+    [Header("SpawnBorders")]
+    [Range(0.0f,-15.0f)]
+    public float leftBorder;
+
+    [Range(0.0f, 15.0f)]
+    public float rightBorder;
+
+
+    [Header("References")]
     public GameObject player;
     public Transform playerTransform;
     public GameObject bulletInstance;
     public Vector3 playerpos;
+
+    bool isActive;
     
-
-    private float TargetTime = 5f;
-    private float spawnTime = 5f;
-
-    private bool isActive;
-    public float NewXPos;
-    public float NewXNeg;
-    
-    public float IncrementingX;
-    // Use this for initialization
-
-
     void Start () {
         player = GameObject.Find("prefab_player");
-        //playerpos = player.transform.position;
         bulletInstance = Resources.Load<GameObject>("prefab_bullet");
         isActive = false;
-        transform.position = new Vector3(Random.Range(-9.8f, 9.8f), 50, -10);
-
+        transform.position = new Vector3(Random.Range(leftBorder, rightBorder), 50, -10);
+        RespawnTime = spawnTime;
+        TargetTime = DurationTime;
     }
 
-    private void OnTriggerEnter(Collider other)
-    {
-        if (other.gameObject.name == "BorderController")
-        {
+    private void OnTriggerEnter(Collider other){
+        if (other.gameObject.name == "BorderController"){
             isActive = true;
-            Debug.Log("hit");
-        }
+             }
     }
-
-
+    
     // Update is called once per frame
     void Update()
     {
         playerpos = player.transform.position;
+            if (!isActive){ RespawnTime -= Time.deltaTime;
+            Debug.Log(RespawnTime + "  " + "Until respawn");}
 
-       
+                if (transform.position.y < YbeforeRespawn){
+                    transform.position = new Vector3(Random.Range(leftBorder, rightBorder), HeightOnRespawn, -10);
+                     RespawnTime = spawnTime;
+                }
+
+        if (RespawnTime <= 0)
+        {
+            transform.position = new Vector3(transform.position.x, transform.position.y - fallingSpeed, transform.position.z);
+            Debug.Log("respawn");
+        }
         
-
-        if (!isActive)
-        {
-            spawnTime -= Time.deltaTime;
-            Debug.Log("time before respawn" + "  " + spawnTime);
-
-        }
-
-        if (transform.position.y < -50)
-        {
-            transform.position = new Vector3(Random.Range(-6.0f, 6.0f), 50, -10);
-            spawnTime = 5f;
-        }
-
-        //if (TargetTime <= 0)
-        //{
-        //    transform.position = new Vector3(Random.Range(-6.0f, 6.0f), 50, -10);
-        //}
-
-
-        if (spawnTime <= 0)
-            transform.position = new Vector3(transform.position.x, transform.position.y - 0.1f, transform.position.z);
-
-
-        if (isActive)
-        {
+        if (isActive){
             TargetTime -= Time.deltaTime;
-            Debug.Log("time before reposition" + "  " + TargetTime);
 
+                Debug.Log(DurationTime + "  " + "time before reposition" );
+            
+                if (playerpos.x > 0f)
+                    transform.position = new Vector3(playerpos.x - 1f, playerpos.y -1, playerpos.z);
+                else if (playerpos.x < 0f)
+                    transform.position = new Vector3(playerpos.x + 1f, playerpos.y- 1, playerpos.z);
+                else
+                   transform.position = new Vector3(playerpos.x, playerpos.y + 2f, playerpos.z);
 
-
-
-            if (playerpos.x > 0f)
-                transform.position = new Vector3(playerpos.x - 1f, playerpos.y -1, playerpos.z);
-           else if (playerpos.x < 0f)
-                transform.position = new Vector3(playerpos.x + 1f, playerpos.y- 1, playerpos.z);
-            else
-               transform.position = new Vector3(playerpos.x, playerpos.y + 2f, playerpos.z);
-
-            if (TargetTime <= 0.0f)
-            {
-                TimerEnded();
-            }
+                if (TargetTime <= 0.0f){
+                    TimerEnded();
+                }
         }
-                 
-
-
+        
         if (Input.GetButton("Shoot") && isActive)
-        {
             shoot();
-        }
         
     }
 
     void shoot()
     {
-        if (player.GetComponent<Player>().fuel >= 1)
-        {
-            player.GetComponent<Player>().fuel--;
+        if (player.GetComponent<Player>().fuel >= 1) { 
+                player.GetComponent<Player>().fuel--;
         
-
         GameObject bullet = Instantiate(
                 bulletInstance,
                 transform.position,
-               transform.rotation);
-        bullet.GetComponent<Bullet>().bulletType = player.GetComponent<Player>().GetCellType;
-        bullet.GetComponent<Bullet>().speed = 30f;
-        bullet.GetComponent<Bullet>().x = -100;
+                transform.rotation);
+            bullet.GetComponent<Bullet>().bulletType = player.GetComponent<Player>().GetCellType;
+            bullet.GetComponent<Bullet>().speed = 30f;
+            bullet.GetComponent<Bullet>().x = -100;
         }
      }
 
@@ -122,9 +114,9 @@ public class Companion : MonoBehaviour {
         {
 
         isActive = false;
-        TargetTime = 5;
-        transform.position = new Vector3(Random.Range(-6.0f,6.0f), 50, -10);
-        spawnTime = 5f;
+        TargetTime = DurationTime;
+        transform.position = new Vector3(Random.Range(leftBorder, rightBorder), HeightOnRespawn, -10);
+        RespawnTime = spawnTime;
         }
 
         
---
+++ Assets/Scripts/Rotator.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Rotator : MonoBehaviour {
+    [Range(5.0f,150.0f)]
+    public float speed = 5.0f;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        transform.Rotate(0,speed *Time.deltaTime,0);
+	}
+}
